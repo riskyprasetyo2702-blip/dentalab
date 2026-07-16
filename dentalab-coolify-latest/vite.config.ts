f73fd8898e1261a1c9a50765d55782d886440e1c
riskyprasetyo2702-blip <riskyprasetyo2702@gmail.com>
2026-07-16 16:16:34 +0700

    Delete dentalab-coolify-latest directory

--- dentalab-coolify-latest/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import vinext from "vinext";
-import { defineConfig } from "vite";
-import { sites } from "./build/sites-vite-plugin";
-
-export default defineConfig({
-  plugins: [vinext(), sites()],
-});
